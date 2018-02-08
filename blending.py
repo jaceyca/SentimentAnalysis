471d1adf67a3d5f8cab19703036666e14301628e
anniewang1914 <Annie@Annies-MacBook-Pro.local>
2018-02-08 02:30:27 -0800

    Updated blending

--- blending.py
+++ blending.py
@@ -1,5 +1,8 @@
 import numpy as np
+import pathlib
+from datetime import datetime
 
+# classifiers
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, ExtraTreesClassifier, RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import LogisticRegression, PassiveAggressiveClassifier, RidgeClassifier, SGDClassifier
 from sklearn.linear_model import LogisticRegressionCV, RidgeClassifierCV
@@ -9,8 +12,13 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.preprocessing import Normalizer
+
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.cross_validation import KFold
 from sklearn.model_selection import GridSearchCV, ShuffleSplit
+from sklearn.preprocessing import Normalizer
+from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
 
 
 
@@ -86,6 +94,39 @@ def normalization(X_train, X_test):
     return (train_norm, test_norm)
 
 
+def percentError(yPred, yTrue):
+    '''
+    Calculates the percent error between two given label sets
+    
+    Inputs:
+        yPred: predicted labels
+        yTrue: actual labels
+    
+    Outputs:
+        error: float of the number of mismatches divided by total length
+    '''     
+    return 1.0-np.sum(np.equal(yPred, yTrue))/len(yTrue)
+
+
+
+def save_data(data, filename="%s.txt" % datetime.today().strftime("%X").replace(":", "")):
+    '''
+    Function to save the predictions by the classifier
+    
+    Inputs: predictions, (optional) filename
+        If filename isn't specified, then it just uses the current time
+    
+    Outputs: Does not return anything
+        Writes the submisssion to a textfile that should have the same format as the sample_submission.txt
+    '''
+    
+    # Creates a new submissions folder if one doesn't exist
+    pathlib.Path('submissions').mkdir(parents=True, exist_ok=True)
+    with open("submissions\\%s" % filename, "w") as f:
+        f.write("Id,Prediction\n")
+        for Id, prediction in enumerate(data, 1):
+            string = str(Id) + ',' + str(prediction) + '\n'
+            f.write(string)
 
 
 
@@ -103,14 +144,18 @@ trainX, trainY, testX, testY = split_data(X_train_n, y_train)
 
 # PUT THE THINGS WE WANT TO BLEND HERE.
 test1 = LogisticRegression(C=2.7825594)
-# test2 = LogisticRegression(C=2.7825594)
-test2 = MLPClassifier(activation = 'logistic', hidden_layer_sizes=(300,))
-test3 = RidgeClassifier(alpha=0.35448276)
-testSV = SVC(gamma=1, C=1)
+test2 = SVC(gamma=1, C=2, probability = True)
+test3 = MLPClassifier(activation = 'logistic', hidden_layer_sizes=(300,))
 
-blend = VotingClassifier(estimators=[('lr', test1), ('etc', test2), ('r', test3)], voting='hard')
 
-for clf, label in zip([test1, test2, test3, blend], 
-                      ['Logistic Regression', 'MLP Classifier', 'Ridge Classifier', 'Ensemble']):
-    scores = cross_val_score(clf, X_train_n, y_train, cv=5, scoring = 'accuracy')
-    print("Accuracy: %0.8f (+/- %0.8f) [%s]" % (scores.mean(), scores.std(), label))
+blend = VotingClassifier(estimators=[('lr', test1), ('svc', test2), ('mlp', test3)], voting='soft')
+
+blend.fit(X_train_n, y_train)
+predictions = blend.predict(X_test)
+
+save_data(predictions)
+
+# for clf, label in zip([test1, test2, test3, blend], 
+#                       ['Logistic Regression', 'LogReg', 'MLPClass', 'Ensemble']):
+#     # scores = cross_val_score(clf, X_train_n, y_train, cv=5, scoring = 'accuracy')
+#     print("Accuracy: %0.8f (+/- %0.8f) [%s]" % (scores.mean(), scores.std(), label))
